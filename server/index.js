65d9d6dae01d2c18ae459c63db4f936868b09f85
cankirism <cankiri.bilsis@saglik.gov.tr>
2024-02-08 08:37:03 +0300

    open ai post added

--- server/index.js
+++ server/index.js
@@ -9,43 +9,41 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.resolve(__dirname, './build')));
 
-// const openai = new OpenAI({
-// 	apiKey: process.env.OPENAI_API_KEY,
-// });
+const openai = new OpenAI({
+	apiKey: process.env.OPENAI_API_KEY,
+});
 app.use('/',async(req,res)=>{
 
 	res.send("server is running ")
 })
-// app.get('/api',async(req,res)=>{
-// 	res.status(200).json({success:"başarılı"});
-// })
-// app.post('/api', async (req, res) => {
-//    debugger;
-// 	try {
+
+app.post('/api', async (req, res) => {
+   debugger;
+	try {
 		
-// 		console.log("api side",req.body);
-// 		const { prompt } = req.body;
-// 		console.log(prompt);
-// 		const response = await openai.images.generate({
-// 			prompt,
-// 			n: 1,
-// 			size: "512x512",
-// 			response_format: 'b64_json',
-// 		});
-// 		// const response = await openai.createImage({
+		console.log("api side",req.body);
+		const { prompt } = req.body;
+		console.log(prompt);
+		const response = await openai.images.generate({
+			prompt,
+			n: 1,
+			size: "512x512",
+			response_format: 'b64_json',
+		});
+		// const response = await openai.createImage({
 
-// 		// 	prompt:prompt,
-// 		// 	n:1,
-// 		// 	size:"512x512",
-// 		// });
-// 		console.log(response);
-// 		const image = response.data[0].b64_json;
-// 		res.status(200).json({ photo: image });
+		// 	prompt:prompt,
+		// 	n:1,
+		// 	size:"512x512",
+		// });
+		console.log(response);
+		const image = response.data[0].b64_json;
+		res.status(200).json({ photo: image });
 
-// 	}
-// 	catch (error) { console.log("hata olustu",error); }
+	}
+	catch (error) { console.log("hata olustu",error); }
 
-// });
+});
 app.listen(5000,()=>console.log("server staerted on port 5000"));
 
 
