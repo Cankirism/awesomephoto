bcac2d03102900cf6b4b0c0d6f2b4ae8136b7a83
cankirism <cankiri.bilsis@saglik.gov.tr>
2024-02-08 09:07:15 +0300

    nothing

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const options = [
 app.use(options);
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static(path.resolve(__dirname, './build')));
-
+console.log("hi..");
 const openai = new OpenAI({
 	apiKey: process.env.OPENAI_API_KEY,
 });
