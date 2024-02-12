742d18af7389075eb02c83b9b4d79fae073ce503
cankirism <cankiri.bilsis@saglik.gov.tr>
2024-02-12 10:52:34 +0300

    client favicon and logo updated

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import Logo from '../assets/1.png'
 
 const Header = () => {
     return (
-        <img src={Logo} alt="Kinsta Logo" className="kinsta-logo" />
+        <img src={Logo} alt="photoai Logo" className="photoai-logo" />
     )
 }
 
